9145a27b13d2c4bc1d2725e1f34382faf1c7c823
Hrugved <chrugved@gmal.com>
2021-03-27 10:31:48 +0530

    initial commit

---
+++ ecommerceapi/src/main/java/com/xpanxion/EcommerceapiApplication.java
@@ -0,0 +1,13 @@
+package com.xpanxion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceapiApplication.class, args);
+	}
+
+}
---
+++ ecommerceapi/src/test/java/com/xpanxion/EcommerceapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpanxion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
