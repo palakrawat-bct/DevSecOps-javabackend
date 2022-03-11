246bc2281ffebb386a3819a2c0fef9b40958f5d0
Jenkins Automation Server <jenkins@ip-172-31-16-253.us-west-1.compute.internal>
2022-03-11 09:25:20 +0000

    Added by angular

--- ecommerceapi/src/main/resources/static/polyfills.js
+++ ecommerceapi/src/main/resources/static/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /var/lib/jenkins/workspace/DevSecOps/DevSecOps/ecommerce/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /var/lib/jenkins/workspace/demoapp/DevSecOps/ecommerce/src/polyfills.ts */"hN/g");
 
 
 /***/ }),
