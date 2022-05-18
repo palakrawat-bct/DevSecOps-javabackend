671ad30d0719d61c4c6b6e233498ab48ec977c59
hrugved.chavan <hrugved.chavan@blueconchtech.com>
2022-05-18 18:11:38 +0530

    added global cors config

--- ecommerceapi/src/main/java/com/xpanxion/EcommerceapiApplication.java
+++ ecommerceapi/src/main/java/com/xpanxion/EcommerceapiApplication.java
@@ -2,12 +2,25 @@ package com.xpanxion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcommerceapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceapiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/server").allowedOrigins("http://localhost:8081");
+			}
+		};
 	}
 
 }
